d5a1f2b0d96245ff1c6ff9dafe2ea4842efbb50f
Tales Oliveira <conta.tales.oliveira2016@gmail.com>
2020-11-03 20:20:06 -0300

    projeto criado

---
+++ src/test/java/com/estacionamento/beca/ProjetoEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estacionamento.beca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
